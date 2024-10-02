e2f7f146fa2272a16800fb5888d31210b129cce4
Bartlomiej Gajewski <bartlomiej_gajewski@epam.com>
2024-10-02 12:33:27 +0200

    Initital

---
+++ src/main/java/com/aws/snapstart/demo/snapstart/handler/GetReportFunction.java
@@ -0,0 +1,16 @@
+package com.aws.snapstart.demo.snapstart.handler;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+public class GetReportFunction implements Function<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    @Override
+    public APIGatewayProxyResponseEvent apply(APIGatewayProxyRequestEvent apiGatewayProxyRequestEvent) {
+        return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody("OK");
+    }
+}
